dde8bd5a00cf1244c1a1a10deedda75bfe85ce5a
taanhphuong <79266495+taanhphuong@users.noreply.github.com>
2021-02-18 22:13:37 +0700

    config.php

--- config.php
+++ config.php
@@ -5,15 +5,15 @@
 	ob_start();
 
 	$db_host = "localhost";
-	$db_user = "root";
-	$db_pass = "";
+	$db_user = "phuongdz";
+	$db_pass = "habeou";
 	$db_data = "test";
 
 	$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_data);
 	mysqli_set_charset($connect, 'utf8');
 
 	$title = "Bán Acc Game - Shop Nick Giá Rẻ - Uy Tín Tại Việt Nam";
-	$home_url = "http://localhost";
+	$home_url = "http://phuong360.tk";
 
 
 ?>
